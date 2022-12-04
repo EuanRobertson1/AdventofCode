d215174b5fcc70c5a12f7d779b9bd4c2f134f80e
EuanRobertson1 <92741298+EuanRobertson1@users.noreply.github.com>
2022-12-04 18:45:01 +0000

    Update Day4.py

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -10,22 +10,19 @@ def readFromFile():
     return con
 
 def checkForOverlap(o,e1, e2):
-    
-    
-    
-    #e1 overlapping e2 at the start/e2 overlapping e1 at the end
-    if(int(e1[0])<= int(e2[0]) and int(e1[1]) >= int(e2[0])):
-        o+= 1
-    #e2 overlapping e1 at the start/e1 overlapping e2 at the end
-    elif(int(e2[0]) <= int(e1[0]) and int(e2[1]) >= int(e1[0])):
-        o+= 1
-    
-    
-    return o
+  if(int(e1[0])<= int(e2[0]) and int(e1[1]) >= int(e2[0]) or int(e2[0]) <= int(e1[0]) and int(e2[1]) >= int(e1[0])):
+    o+= 1
+  return o
 
-def part1and2():
+def checkForContainment(c,e1,e2):
+  if(int(e1[0]) >= int(e2[0]) and int(e1[1]) <= int(e2[1]) or int(e2[0]) >= int(e1[0]) and int(e2[1]) <= int(e1[1])):
+   c+=1
+  return c
+
+
+def main():
  #variables
- fullyContained = 0
+ ContainmentSum = 0
  i = 0
  content = readFromFile()
  overlapSum = 0
@@ -41,25 +38,18 @@ def part1and2():
   elf2 = pair[1].split('-')
 
   #check if either range is fully contained within the other
-  #elf1 range is inside elf2
-  if(int(elf1[0]) >= int(elf2[0]) and int(elf1[1]) <= int(elf2[1])):
-    fullyContained+=1
-  #elf2 range is inside elf1
-  elif(int(elf2[0]) >= int(elf1[0]) and int(elf2[1]) <= int(elf1[1])):
-    fullyContained+=1 
-
+  ContainmentSum = checkForContainment(ContainmentSum, elf1, elf2)
+  
   #check for overlapping pairs
   overlapSum = checkForOverlap(overlapSum,elf1, elf2)  
 
   #increase counter
   i+=1
- 
 
- 
- print("According to Part 1, the number of pairs fully contained within the other is: " + str(fullyContained))
+ print("According to Part 1, the number of pairs fully contained within the other is: " + str(ContainmentSum))
  print("According to Part 2, the number of pairs overlapping is : " + str(overlapSum))
  
 #run the function
-part1and2()
+main()
 
 
