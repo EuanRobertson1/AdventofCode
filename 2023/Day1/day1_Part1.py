fc199bd2ee2c748fcc43f4ca3b76e160eef43c47
Nuke242 <2463967@dundee.ac.uk>
2023-12-01 20:07:13 +0000

    added 2023 files

---
+++ 2023/Day1/day1_Part1.py
@@ -0,0 +1,56 @@
+import array
+
+#vars
+curLn = 0
+sum = 0 
+fullNum = ""
+
+
+
+#file handling
+file = open('Day1/input.txt')
+fileCont = file.readlines()
+
+#loop to end
+while curLn < len(fileCont):
+
+    lineCont = fileCont[curLn].strip()
+
+    #find the digits
+    i = 0
+    firstDigit = 0
+    lastDigit = 0
+
+    while i < len(lineCont):
+        if lineCont[i].isdigit():
+            if firstDigit == 0:
+                firstDigit = lineCont[i]
+            else:
+                lastDigit = lineCont[i]
+        
+        
+        i +=1
+    
+    #check if only one digit is in line
+    if lastDigit == 0:
+        lastDigit = firstDigit
+
+
+    #combine 
+    fullNum = str(firstDigit) + str(lastDigit)
+
+    #add to sum
+    sum += int(fullNum)
+    
+
+    #fix vars
+    curLn += 1
+    
+
+file.close()
+
+print("Sum of all calibration values is " + str(sum))
+    
+            
+
+
---
+++ 2023/Day1/day1_Part2.py
@@ -0,0 +1,93 @@
+import array
+
+#vars
+curLn = 0
+sum = 0 
+fullNum = ""
+
+
+stringNums = ["one","two","three","four","five","six","seven","eight","nine"]
+
+stringNumChecker = ""
+
+#file handling
+file = open('Day1/input.txt')
+fileCont = file.readlines()
+
+#loop to end
+while curLn < len(fileCont):
+
+    lineCont = fileCont[curLn].strip()
+    
+    #find the digits
+    i = 0
+    firstDigit = 0
+    lastDigit = 0
+
+    while i < len(lineCont):
+
+        
+        stringNumChecker += lineCont[i]
+        
+        
+        x = 0
+        #check if current str held in stringNumCheck matches any of the  values in each stringNums index
+        while x < len(stringNums):
+            if stringNums[x] in stringNumChecker:
+               if firstDigit == 0:
+                firstDigit = x +1
+                
+               else:
+                lastDigit = x +1
+                
+               
+               stringNumChecker = ""
+
+               #allows for shared letters for word numbers
+               i-=1
+               
+               
+                
+            
+            x+=1
+        
+        if lineCont[i].isdigit():
+            #reset stringNumChecker
+            stringNumChecker = ""
+
+            if firstDigit == 0:
+                firstDigit = lineCont[i]
+                
+            else:
+                lastDigit = lineCont[i]
+                
+        
+        
+
+        i +=1
+    
+    #check if only one digit is in line
+    if lastDigit == 0:
+        lastDigit = firstDigit
+
+
+    #combine 
+    fullNum = str(firstDigit) + str(lastDigit)
+    print(fullNum)
+
+    #add to sum
+    sum += int(fullNum)
+    
+
+    #fix vars
+    curLn += 1
+    stringNumChecker = ""
+    
+
+file.close()
+
+print("Sum of all calibration values is " + str(sum))
+    
+            
+
+
